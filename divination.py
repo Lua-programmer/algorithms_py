8c67945c67f0b22ccde90a236d6cd9bab9d15599
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-05-17 22:11:27 -0300

    Trabalhando com if e else em python

--- divination.py
+++ divination.py
@@ -1,1 +1,13 @@
+import random
 
+print("************************")
+print("Welcome adivination game")
+print("************************")
+
+number = random.randint(1, 3)
+response = int(input("Enter a number: "))
+
+if response == number:
+    print("Congratulations, you got it right")
+else:
+    print("You missed")
